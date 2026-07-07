a4547c3ceffcea55eb8903b750e293db8e54a6d6
Saumya Singh <saumya0419@gmail.com>
2026-07-07 20:31:13 +0530

    index

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,29 +3,23 @@
 const express = require('express');
 const router = express.Router();
 
-const routes = [
-  ['/auth',          './authRoutes'],
-  ['/users',         './userRoutes'],
-  ['/products',      './productRoutes'],
-  ['/categories',    './categoryRoutes'],
-  ['/cart',          './cartRoutes'],
-  ['/orders',        './orderRoutes'],
-  ['/payments',      './paymentRoutes'],
-  ['/reviews',       './reviewRoutes'],
-  ['/coupons',       './couponRoutes'],
-  ['/admin',         './adminRoutes'],
-  ['/notifications', './notificationRoutes'],
-  ['/blogs',         './blogRoutes'],
-  ['/analytics',     './analyticsRoutes'],
-  ['/videos',        './videoRoutes'],
-  ['/contact',       './contactRoutes'],
-  ['/search',        './searchRoutes'],
-  ['/leads',         './leadRoutes'],
-  ['/interakt',      './interaktRoutes'],
-];
-
-for (const [path, file] of routes) {
-  router.use(path, require(file));
-}
+router.use('/auth',          require('./authRoutes'));
+router.use('/users',         require('./userRoutes'));
+router.use('/products',      require('./productRoutes'));
+router.use('/categories',    require('./categoryRoutes'));
+router.use('/cart',          require('./cartRoutes'));
+router.use('/orders',        require('./orderRoutes'));
+router.use('/payments',      require('./paymentRoutes'));
+router.use('/reviews',       require('./reviewRoutes'));
+router.use('/coupons',       require('./couponRoutes'));
+router.use('/admin',         require('./adminRoutes'));
+router.use('/notifications', require('./notificationRoutes'));
+router.use('/blogs',         require('./blogRoutes'));
+router.use('/analytics',     require('./analyticsRoutes'));
+router.use('/videos',        require('./videoRoutes'));
+router.use('/contact',       require('./contactRoutes'));
+router.use('/search',        require('./searchRoutes'));
+router.use('/leads',         require('./leadRoutes'));
+router.use('/interakt',      require('./interaktRoutes'));
 
 module.exports = router;
